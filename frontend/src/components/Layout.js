e20e237536178b0fb825574cb37df719afe8750b
thain <nguyenthaiduy90@googlemail.com>
2024-04-07 18:45:47 +0200

    added tailwind css

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
+      <nav className="text-3xl font-bold underline">
         <ul>
           <li>
             <Link to="/">Homepage</Link>
